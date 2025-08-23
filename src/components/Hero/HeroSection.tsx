b9b692ec57be3aee23231c41d65a8aac42c05fab
haris ardian <harisfirman387@gmail.com>
2025-08-23 22:59:41 +0700

    Remove unused import of GlassmorphicDotBg in HeroSection component

--- src/components/Hero/HeroSection.tsx
+++ src/components/Hero/HeroSection.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from 'react';
-import GlassmorphicDotBg from '../fragments/GlassmorphicDotBg';
 
 const HeroSection = () => {
   const [currentRole, setCurrentRole] = useState(0);
